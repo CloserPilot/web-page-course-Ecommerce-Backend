6c46508bf508e8e3e9690e86b753884d33ffde97
closerpilot <closerpilot@gmail.com>
2026-02-04 18:19:19 +0000

    Fixing APIs

--- backend/src/controllers/deliveryOptions.controller.js
+++ backend/src/controllers/deliveryOptions.controller.js
@@ -3,7 +3,21 @@ import DeliveryOptions from "../models/deliveryOptions.model.js"
 const getDefaulDeliveryOptions = async (req, res) => {
   try {
     const delivers = await DeliveryOptions.findAll();
-    res.status(200).json(delivers);
+    let response = delivers;
+    const expand = req.query.expand;
+
+    if(expand === 'estimatedDeliveryTime'){
+      response = await Promise.all(delivers.map( async (deliver) => {
+        const deliverMs = Date.now() + deliver.deliveryDays * 24 * 60 * 60 * 1000;
+
+        return{
+          ...deliver.toJSON(),
+          estimatedDeliveryTime: deliverMs
+        }
+      }));
+    }
+
+    res.status(200).json(response);
     
   } catch (error) {
     res.status(500).json({
@@ -11,6 +25,8 @@ const getDefaulDeliveryOptions = async (req, res) => {
     });
   }
 };
+
+
 
 export{
   getDefaulDeliveryOptions
--- backend/src/server.js
+++ backend/src/server.js
@@ -14,12 +14,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.json());
 
-app.use('/products', productRouter);
-app.use('/delivery',deliveryRouter);
-app.use('/cart', cartRouter);
-app.use('/order', orderRouter);
-app.use('/reset',globalRouter);
-app.use('/payment-summary', paymentsSummaryRouter);
+app.use('/api/products', productRouter);
+app.use('/api/delivery',deliveryRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+app.use('/api/reset',globalRouter);
+app.use('/api/payment-summary', paymentsSummaryRouter);
 
 // get the images
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
